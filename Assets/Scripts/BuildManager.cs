ae9dc9c12b6a835eca9e25b9bc57ed7c5ec109ba
stncal <stncallahan@gmail.com>
2018-12-02 13:15:08 -0600

    Added timer, different enemies, and winner screen

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -2,52 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class BuildManager : MonoBehaviour
+{
 
-public class BuildManager : MonoBehaviour {
+    // singleton pattern
+    public static BuildManager instance;
+    private TurretBlueprint turretToBuild;
+    public GameObject standardTurretPrefab;
+    public GameObject laserPrefab;
+    public GameObject missilePrefab;
+    public Text moneyText;
+    private float timeLeft = 60.0f;
+    public Text timeLeftText;
 
-	// singleton pattern
-	public static BuildManager instance;
-	private TurretBlueprint turretToBuild;
-	public GameObject standardTurretPrefab;
-	public GameObject laserPrefab;
-	public GameObject missilePrefab;
-	public Text moneyText;
+    void Awake()
+    {
+        // every time we start the game there will only be one instance of the build manager
+        instance = this;
+    }
 
-	void Awake() {
-		// every time we start the game there will only be one instance of the build manager
-		instance = this;
-	}
+    void Start()
+    {
+        moneyText.text = "$50" + PlayerStats.money.ToString();
+    }
 
-	void Start() {
-		moneyText.text = "$50"  + PlayerStats.money.ToString();
-	}
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("WinnerScreen");
+        }
+        timeLeftText.text = Mathf.Floor(timeLeft % 60).ToString() + " seconds left";
+    }
 
+    // public void SetTurretToBuild(GameObject turret){
+    // 	// change what turret we want to build
+    // 	turretToBuild = turret;
+    // }
 
-	// public void SetTurretToBuild(GameObject turret){
-	// 	// change what turret we want to build
-	// 	turretToBuild = turret;
-	// }
+    public void SelectTurretToBuild(TurretBlueprint turret)
+    {
+        turretToBuild = turret;
+    }
 
-	public void SelectTurretToBuild(TurretBlueprint turret) {
-		turretToBuild = turret;
-	}
+    public bool CanBuild { get { return turretToBuild != null; } }
 
-	public bool CanBuild { get { return turretToBuild != null; } }
+    public void BuildTurretOn(Node node)
+    {
 
-	public void BuildTurretOn(Node node) {
+        if (PlayerStats.money < turretToBuild.cost)
+        {
+            // they don't have enough money to build that turret
+            return;
+        }
 
-		if (PlayerStats.money < turretToBuild.cost){
-			// they don't have enough money to build that turret
-			return;
-		}
+        PlayerStats.money -= turretToBuild.cost;
+        moneyText.text = "$" + PlayerStats.money.ToString();
 
-		PlayerStats.money -= turretToBuild.cost;
-		moneyText.text = "$" + PlayerStats.money.ToString();
+        // build on particular node
+        GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
+        node.turret = turret;
+    }
 
-		// build on particular node
-		GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
-		node.turret = turret;
-	}
+    public void UpgradeWeapons()
+    {
+        if (PlayerStats.money < 100)
+        {
+            return;
+        }
+        PlayerStats.money -= 100;
+        moneyText.text = "$" + PlayerStats.money.ToString();
+    }
 
 
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -3,43 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WaveSpawner : MonoBehaviour {
+public class WaveSpawner : MonoBehaviour
+{
 
-	public Transform enemyPrefab; // enemy prefab we want to spawn
-	public Transform enemyPrefab1;
-	public Transform enemyPrefab2;
-	public float timeBetweenWaves = 5f;	// make longer after testing is done
-	// how long it will take to spawn the first wave
-	private float countdown = 2f;
-	private int waveNumber = 1;
+    public Transform enemyPrefab; // enemy prefab we want to spawn
+    public Transform enemyPrefab1;
+    public Transform enemyPrefab2;
+    public float timeBetweenWaves = 5f; // make longer after testing is done
+                                        // how long it will take to spawn the first wave
+    private float countdown = 2f;
+    private int waveNumber = 1;
 
-	// reference to spawn location
-	public Transform spawnPoint;
+    // reference to spawn location
+    public Transform spawnPoint;
 
-	public Text waveCountdownText;
+    public Text waveCountdownText;
 
-	void Update(){
-		// manage time
-		if (countdown <= 0f){
-			// begin spawning enemies
-			StartCoroutine(SpawnWave());
-			countdown = timeBetweenWaves;
-			return;
-		}
-		countdown -= Time.deltaTime;
-		waveCountdownText.text = "Next wave: " + Mathf.Round(countdown).ToString();
-	}
+    void Update()
+    {
+        // manage time
+        if (countdown <= 0f)
+        {
+            // begin spawning enemies
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            return;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = "Next wave: " + Mathf.Round(countdown).ToString();
+    }
 
-	IEnumerator SpawnWave(){
-		for(int i=0; i<waveNumber; i++){
-			SpawnEnemy();
-			yield return new WaitForSeconds(0.5f);
-		}
-		waveNumber++;
-	}
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveNumber++;
+    }
 
-	void SpawnEnemy(){
-		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
-	}
+    void SpawnEnemy()
+    {
+        int x = Random.Range(1, 4);
+        if (x == 1)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+        else if (x == 2)
+        {
+            Instantiate(enemyPrefab1, spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            Instantiate(enemyPrefab2, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
 
 }
